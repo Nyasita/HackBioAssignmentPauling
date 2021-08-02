66e588ccb6e3214ec47efb72c7a72329b45ee355
Pravitha K Sivanandan <74097867+Pravithaks@users.noreply.github.com>
2021-08-02 23:05:30 +0530

    Add files via upload

---
+++ PravithaKS.r
@@ -0,0 +1,8 @@
+#Print my name
+print ("Pravitha KS")
+#Print mail ID
+print ("pravithaksivanandan@gmail.com")
+#print Slack username @Team name
+print ("Pravithaksivanandan @Pauling")
+#print Biostack
+print ("Drug development")
