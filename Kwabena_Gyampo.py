bae8e472762a6938436fe0f863b1ff658c765523
Gyamps <dentukwabena.kg@gmail.com>
2021-08-03 11:22:22 +0000

    First Assignment

---
+++ Kwabena_Gyampo.py
@@ -0,0 +1,11 @@
+# Name: First and Last
+print('Name: Kwabena Gyampo')
+
+# Email
+print('Email: dentukwabena.kg@gmail.com')
+
+# Slack Username
+print('Slack Username: @Kwabs')
+
+# Biostack
+print('Biostack: Genomics')
