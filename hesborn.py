c4c4f27561413c00832cf3ee1bb17d923fc5cc41
hesbornomwandho <72735085+hesbornomwandho@users.noreply.github.com>
2021-08-03 08:34:15 +0300

    Add files via upload

---
+++ hesborn.py
@@ -0,0 +1,7 @@
+print("My name is Hesborn obura")
+
+print ('My email address is hesbornomwandho@gmail.com')
+
+print ("My slack name is @hesborn")
+
+print ("My biostack of choice is Vaccine developmemt")
