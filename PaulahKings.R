eac9e9a10cf25163678990090fc19266aee48ef3
Paulahkings <61578503+Paulahkings@users.noreply.github.com>
2021-08-04 18:54:18 +0300

    Add files via upload

---
+++ PaulahKings.R
@@ -0,0 +1,9 @@
+#brief introduction
+
+cat("My name is Pauline King'ori.")
+
+cat("My email is paulinewangeci3@gmail.com.")
+
+cat("My Slack username is @PaulahKings.")
+
+cat("My biostack is Genomics.")
