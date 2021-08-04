ac3af76820c24890ed4e303816132980fdd4dca6
Pravitha K Sivanandan <74097867+Pravithaks@users.noreply.github.com>
2021-08-04 23:34:55 +0530

    Delete PravithaKS.r

--- PravithaKS.r
+++
@@ -1,8 +0,0 @@
-#Print my name
-print ("Pravitha KS")
-#Print mail ID
-print ("pravithaksivanandan@gmail.com")
-#print Slack username @Team name
-print ("Pravithaksivanandan @Pauling")
-#print Biostack
-print ("Drug development")
