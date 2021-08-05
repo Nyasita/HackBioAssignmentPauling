43eedc05d82c580d3b0c4e55158d2200423c41bf
Elishama-Yomi <88346095+Elishama-Yomi@users.noreply.github.com>
2021-08-05 15:41:02 +0100

    Update Elishama .py

--- Elishama .py
+++ Elishama .py
@@ -1,18 +1,8 @@
 from scipy.spatial.distance import hamming
-name = "Elishama Yomi-Agbajor"
-email_address = "elishamayomiagbajor@gmail.com"
-slack_username = "@SilveryBubble"
-biostack = "Public health and Genomic Epidemiology"
-twitter_handle = "@Elishama_Yomi"
-print(name)
-print(email_address)
-print(slack_username)
-print(biostack)
-print(twitter_handle)
 # Calculate hamming distance
-#define arrays
-str1 = ['@', 'S', 'i', 'l', 'v', 'e', 'r', 'y', 'B', 'u', 'b', 'b', 'l', 'e']
-str2 = ['@', 'E', 'l', 'i', 's', 'h', 'a', 'm', 'a', '_', 'Y', 'o', 'm', 'i']
-#calculate Hamming distance between the two arrays
-hamming_distance = hamming(str1, str2) * len(str1)
-print(hamming_distance)
+# define arrays
+slack_username = ['@', 'S', 'i', 'l', 'v', 'e', 'r', 'y', 'B', 'u', 'b', 'b', 'l', 'e']
+twitter_handle = ['@', 'E', 'l', 'i', 's', 'h', 'a', 'm', 'a', '_', 'Y', 'o', 'm', 'i']
+# calculate Hamming distance between the two arrays
+hamming_distance = hamming(slack_username, twitter_handle) * len(slack_username)
+print("Elishama Yomi-Agbajor, elishamayomiagbajor@gmail.com, @SilveryBubble, Public health and Genomic Epidemiology, @Elishama_Yomi, 13.0")
