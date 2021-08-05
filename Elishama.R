2a70e90a8c3b6586931368ea892f5c0f1bc92c91
Elishama-Yomi <88346095+Elishama-Yomi@users.noreply.github.com>
2021-08-05 14:48:54 +0100

    Delete Elishama.R

--- Elishama.R
+++
@@ -1,7 +0,0 @@
-print("Name: Elishama Yomi-Agbajor")
-
-print("Email address: elishamayomiagbajor@gmail.com")
-
-print("Slack username: @Elishama")
-
-print("Biostack: Public health and Genomic Epidemiology")
