08f2e3e9ff2581e0582b1f56f5f0105e1e843235
Paulahkings <61578503+Paulahkings@users.noreply.github.com>
2021-08-05 10:17:38 +0300

    Update PaulahKings.R

--- PaulahKings.R
+++ PaulahKings.R
@@ -1,9 +1,10 @@
+#!/usr/bin/Rscript
+
+
 #brief introduction
+cat("Name:Pauline King'ori")
+cat("Email:paulinewangeci3@gmail.com")
+cat("Slack:@PaulahKings")
+cat("Biostack:Genomics")
+cat("Twitter:@paulah_kings")
 
-cat("My name is Pauline King'ori.")
-
-cat("My email is paulinewangeci3@gmail.com.")
-
-cat("My Slack username is @PaulahKings.")
-
-cat("My biostack is Genomics.")
