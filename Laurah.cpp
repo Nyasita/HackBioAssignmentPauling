6d997399eca8eee5a4fbc972f981f69b6dc90116
Nyasita <72727098+Nyasita@users.noreply.github.com>
2021-08-05 21:41:19 +0300

    C file

---
+++ Laurah.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "My name is Laurah Ondari aka Laurah on slack. \n" << endl;
+    cout << "I'm interested in genomics, drug development and data analytics. \n " << endl;
+    cout << "Chat me up on email : laurahondari22@gmail.com  " << endl;
+
+    return 0;
+}
