54ecee530aa94c8e1f1401aefa8743de9b53269c
Pravitha K Sivanandan <74097867+Pravithaks@users.noreply.github.com>
2021-08-05 20:50:29 +0530

    Update PravithaKS.r

--- PravithaKS.r
+++ PravithaKS.r
@@ -1,3 +1,4 @@
+HackBio Firsttask
 #Print my name
 print ("Pravitha KS")
 #Print mail ID
@@ -11,3 +12,16 @@ print ("Pravisivanandan")
 #calculating Hamming distance between <slack_handle> and <twitter_handle>
 library(stringdist)
 stringdist("Pravithaks10277","Pravisivanandan", method = 'hamming',nthread = getOption("sd_num_thread"))
+#print name, email, slack_handle, biostack, twitter_handle, hamming_distance 
+print (paste ("Pravitha KS","pravithaksivanandan@gmail.com","@Pravithaks10277@Pauling","Drug development","@Pravisivanandan","10"))
+df <- data.frame(Name = c("Pravitha KS"),
+                 Email = c("pravithaksivanandan@gmail.com"),
+                 Slack_handle = c("@Pravithaks10277@Pauling"),
+                 Biostack = c("Drug development"),
+                 Twitter_handle = c("@Pravisivanandan"),
+                 Hamming_distance = c(10)
+)
+print (df)
+write.table(df,file= "Exportedfirsttaskhackbio.csv", row.names = F, sep = ',' )
+dev.off()
+################################################
