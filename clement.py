843cd01c1e996eea5c203f815f62a304a82c690e
clementmlay <mlay.clement@gmail.com>
2021-08-05 18:27:37 +0300

    midifief clement.py

--- clement.py
+++ clement.py
@@ -1,8 +1,8 @@
 name = "Clement Mlay"
 email = "mlay.clement@gmail.com"
-slack = "@Clementmlay,"
+slack_username = "@Clementmlay,"
 biostack = "genomics"
 print ("My name is: ",name,".", 
        "My email adress is: ",email,".",
-       "My slack username is: ",slack,".",
+       "My slack username is: ",slack_username,".",
        "My biostack is: ",biostack)
