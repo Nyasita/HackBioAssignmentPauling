45f5006bbdd0b4fc8f3e4149c718c325168fec80
marionnyaboke <45264074+marionnyaboke@users.noreply.github.com>
2021-08-05 17:27:44 +0300

    R script

---
+++ Marion.R
@@ -0,0 +1,9 @@
+
+name <- c("Marion Nyaboke")
+name
+email <- c("marionnyaboke01@gmail.com")
+email
+slack_username <- c("@Marion")
+slack_username
+Biostack <- c("Genomics", "data analytics")
+Biostack
