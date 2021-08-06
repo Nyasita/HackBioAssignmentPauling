3ba77818191fb3768cd9ecb6595f4cc1370d75e7
sonibk <67633694+sonibk@users.noreply.github.com>
2021-08-06 17:06:59 +0300

    python script

--- Brenda.py
+++ Brenda.py
@@ -8,16 +8,14 @@ print("twitter: @Brendaksoni")
 
 print("biostack:Genomics")
 
-from scipy.spartial.distance import hamming
-
 def HammingDistance(slack,twitter):
-  i = 0
-  count =0
-  while(i <len(slack)):
-    if(slack[i] != twitter[i]):
-      count += 1
-    i += 1
- return count
+    if len(slack) > len(twitter):
+        print(len(slack)- len(twitter))
+    elif len(twitter) > len(slack):
+        print(len(twitter)- len(slack))
+    else:
+        print("hamming distance is 0")
+    
 slack = "@Brenda"
 twitter = "@Brendaksoni"
 print( HammingDistance(slack,twitter))
