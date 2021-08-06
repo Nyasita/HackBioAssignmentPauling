2883de08854d6cc0ef3da07e91920afeb44872eb
Ruth-Moraa <72728233+Ruth-Moraa@users.noreply.github.com>
2021-08-06 16:59:10 +0300

    bash script

--- Ruth.sh
+++ Ruth.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-echo "My name is Ruth Moraa"
-echo "My email address is moraaruth33@gmail.com"
-echo "My slack user name is RuthMoraa"
-echo "I'm interested in Genomics"
+Name="Ruth Moraa"
+email_address="moraaruth33@gmail.com"
+slack_username="@RuthMoraa"
+twitter_handle="@moraaruth2"
+biostack="Genomics"
+echo $Name 
+echo $email_address
+echo $slack_username
+echo $twitter_handle
+echo $biostack
+#finding the hamming distance
+if [  email_address =  slack_username ]; then
+    echo "Strings are equal."
+else
+    echo "Strings are not equal."
+fi
+#finding the difference between the string
+emaillength=${#email_address}
+slacklength=${#slack_username}
+hammingdistance=`expr  $emaillength - $slacklength`
+
+echo "The  hamming distance is $hammingdistance"
+
