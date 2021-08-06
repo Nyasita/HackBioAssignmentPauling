eb85ee73d6d1845717acb9429f7ec78fb1ae790a
Paulahkings <61578503+Paulahkings@users.noreply.github.com>
2021-08-06 17:03:38 +0300

    updated introduction

--- PaulahKings.R
+++ PaulahKings.R
@@ -1,10 +1,16 @@
 #!/usr/bin/Rscript
 
+#install and load packages
+install.packages("stringdist")
+library(stringdist)
 
 #brief introduction
-cat("Name:Pauline King'ori")
-cat("Email:paulinewangeci3@gmail.com")
-cat("Slack:@PaulahKings")
-cat("Biostack:Genomics")
-cat("Twitter:@paulah_kings")
+Name <- cat("Pauline King'ori")
+Email <- cat("paulinewangeci3@gmail.com")
+Slack <- cat("@Paulah_Kings")
+Biostack <- cat("Genomics")
+Twitter <- cat("@paulah_kings")
+Hamming_distance <- cat(stringdist("@Paulah_Kings", "@Paulah_Kings", method = 'hamming'))
 
+#print output
+cat(Name,Email,Slack,Biostack,Twitter,Hamming_distance)
