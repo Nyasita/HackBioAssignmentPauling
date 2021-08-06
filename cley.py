533520baa30d4a79386c00e0cd2d4ad693a3bcdd
clementmlay <72735149+clementmlay@users.noreply.github.com>
2021-08-06 16:32:26 +0300

    Update cley.py

--- cley.py
+++ cley.py
@@ -7,8 +7,28 @@ email = 'mlay.clement@gmail.com'
 # Slack Username
 slack_username = '@clementmlay'
 
+# Twiter
+twiter = '@clement_mlay'
+
 # Biostack
 biostack = 'Genomics'
 
 print(f'Name: {name} \nEmail: {email} \
     \nSlack Username: {slack_username} \nBiostack: {biostack}')
+
+# Humming Distance
+from scipy.spartial.distance import hamming
+	
+	def HammingDistance(slack_username,twitter):
+	  i = 0
+	  count =0
+	  while(i <len(slack_username)):
+	    if(first[i] != twitter[i]):
+	      count += 1
+	    i += 1
+	 return count
+	slack = "@clementmlay"
+	twitter = "@clement_mlay"
+	print( HammingDistance(slack_username,twitter))
+	
+
