bd05543326f4dbf4f85c7d7a060a41baf1ce0e42
hesbornomwandho <72735085+hesbornomwandho@users.noreply.github.com>
2021-08-06 16:40:48 +0300

    hesborn.py

--- hesborn.py
+++ hesborn.py
@@ -1,9 +1,32 @@
 Name = "Hesborn Omwandho"
+
 Email = "hesbornomwandho@gmail.com"
-Slack_username = "@hesborn,"
+
+Slack = "@hesborn,"
+
+Twitter = "@hesbornomwandho"
+
 Biostack = "vaccine informatics"
+
 print ("My name is ",Name,",", 
        "My email adress is ",Email,",",
        "My slack username is ",Slack_username,",",
        "My biostack is
        ",Biostack)
+       
+from scipy.spartial.distance import hamming
+       
+def HammingDistance(Slack,Twitter):
+       i=0
+       count =0
+       while(i <len(slack)):
+        if (slack[i] !=Twitter[i]):
+              count += 1
+       return count
+       Slack = "@hesborn"
+       Twitter = "@hesbornomwandho"
+       
+       print (HammingDistance( Slack,twitter))
+       
+       
+ 
