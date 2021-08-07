ca3ef6cf403b986ebad5f898208c3704207a18a8
SuliJimoh <88276401+SuliJimoh@users.noreply.github.com>
2021-08-07 03:22:54 +0100

    Delete suliat_hackbio_1.m

--- suliat_hackbio_1.m
+++
@@ -1,8 +0,0 @@
-name = ['Suliat Abike Jimoh'];
-email = ['suliatjimoh@gmail.com'];
-slackusername = ['@Sulijimoh'];
-biostack = ['genomics'];
-twitter_username = ['sulistical'];
-diff = [sum(slackusername ~= twitter_username)];
-disp([name; email; slackusername; biostack; twitter_username;])
-hamming_distace = diff
