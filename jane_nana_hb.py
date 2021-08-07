a2abc52e36d6736a28de579e597712302ddf6cd6
Gyamps <dentukwabena.kg@gmail.com>
2021-08-07 20:39:36 +0000

    Fixed while loop

--- jane_nana_hb.py
+++ jane_nana_hb.py
@@ -3,14 +3,14 @@ email_address = "jaynenana18@gmail.com"
 slack_username = "@nana"
 twitter = "@aara"
 biostack = "Genomics"
-def hammingDist (str1, str2):
+def hammingDist(str1, str2):
     i = 0
     count = 0
     while (i < len(str1)):
         if (str1[i] != str2[i]):
             count += 1 
-            i += 1
-            return count
+        i += 1
+    return count
             
 str1 = "@nana"
 str2 = "@aara"
